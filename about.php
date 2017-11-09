b8bc776c81540c79a1add6ecdf10bce993b19bb0
Zachary Glaser <glaserfirm@rainier.dreamhost.com>
2017-11-08 16:58:07 -0800

    added attorneys to about page

--- about.php
+++ about.php
@@ -3,110 +3,137 @@ include 'garble_cnfg.php';
 header_check();
 ?>
   <section class="container">
-    <div class="row">
-      <div class="col-md-2">
+    <div class="col-md-9 mainFees">
+      <div class="row">
+        <div class="col-md-2">
+        </div>
+        <div class="col-md-8">
+          <h2 class="text-center">Go from Request to Results in 6 easy steps:</h2>
+          <h4 class="text-center">Let our experienced attorneys help you have a presence throughout the entire state of Tennessee</h4>
+        </div>
+        <div class="col-md-2">
+        </div>
       </div>
-      <div class="col-md-8">
-        <h2 class="text-center">Go from Request to Results in 6 easy steps:</h2>
-        <h4 class="text-center">Let our experienced attorneys help you have a presence throughout the entire state of Tennessee</h4>
-      </div>
-      <div class="col-md-2">
+      <hr>
+      <div class="row no-gutters list">
+        <div class="col-md-12">
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-10 outline">
+            <div class="col-md-1 mt-0">
+              <h1 class="big-text">1</h1>
+            </div>
+            <div class="col-md-11 mt-0">
+              <h3 class="text-left"> &#128198; Find a Court Date: </h3>
+              <h4>You can look through our schedule of appearances on our Calendar page, where you can seach either by Court or by Date. We generally appear in each court once every three months at this time, although larger Courts are seen more regularly. Have a look and see if our dates are convenient for you.</h4>
+              <a href="calendar.php" class="btn btn-primary">Search Calendar</a>
+            </div>
+          </div>
+          <div class="col-md-1">
+          </div>
+        </div>
+        <div class="col-md-12">
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-10 outline">
+            <div class="col-md-1 mt-0">
+              <h1 class="big-text">2</h1>
+            </div>
+            <div class="col-md-11 mt-0">
+              <h3 class="text-left"> &#10004; Make a Request:</h3>
+              <h4>Once you have found a convenient appearance, select the date to see the particular information for that appearance. From there, you can easily make a request to our office to confirm availability. You will immediately get a receipt email and should receive confirmation within one business day.</h4>
+            </div>
+          </div>
+          <div class="col-md-1">
+          </div>
+        </div>
+
+        <div class="col-md-12">
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-10 outline">
+            <div class="col-md-1">
+              <h1 class="big-text">3</h1>
+            </div>
+            <div class="col-md-11 mt-0">
+              <h3 class="text-left"> <i class="fa fa-file-text-o" aria-hidden="true"></i> Set your cases:</h3>
+              <h4>After you have received confirmation from our office, make sure that you are using the proper warrant for your specific Court, then simply set your court date for the day you have confirmed with our office. Since you have confirmed the appearance with our office, we will notify you of any changes with the court date if any arise.</h4>
+            </div>
+          </div>
+          <div class="col-md-1">
+          </div>
+        </div>
+        <div class="col-md-12">
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-10 outline">
+            <div class="col-md-1 mt-0">
+              <h1 class="big-text">4</h1>
+            </div>
+            <div class="col-md-11 mt-0">
+              <h3 class="text-left"> 	<i class="fa fa-paper-plane" aria-hidden="true"></i> Send your docket:</h3>
+              <h4>At least two business days prior to our appearance on your behalf, please send us your finalized docket so our attorney can prepare for the court date. We do understand that sometimes the particulars of cases change between that time and the court date. You will be able to contact our office directly in that time period to notify us of the changes if necessary.</h4>
+
+            </div>
+          </div>
+          <div class="col-md-1">
+          </div>
+        </div>
+        <div class="col-md-12">
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-10 outline">
+            <div class="col-md-1">
+              <h1 class="big-text">5</h1>
+            </div>
+            <div class="col-md-11 mt-0">
+              <h3 class="text-left"> <i class="fa fa-gavel" aria-hidden="true"></i> Day of Court:</h3>
+              <h4>On the day of court, your office does not need to take any active steps to make sure your docket is handled appropriately. However, our attorney will have your office's information should they need to contact you for settlement or payment arrangement approval.</h4>
+            </div>
+          </div>
+          <div class="col-md-1">
+          </div>
+        </div>
+        <div class="col-md-12">
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-10 outline">
+            <div class="col-md-1">
+              <h1 class="big-text">6</h1>
+            </div>
+            <div class="col-md-11 mt-0">
+              <h3 class="text-left"> <i class="fa fa-reply" aria-hidden="true"></i> Get Results:</h3>
+              <h4>After we have appeared for your firm, we will compile the results from court and email them to your office. We know that this is a time sensitive matter and usually you will see your results within two business days. At this time, an invoice will be sent to you for our sevices.</h4>
+            </div>
+          </div>
+          <div class="col-md-1">
+          </div>
+        </div>
       </div>
     </div>
-    <hr>
-    <div class="row no-gutters list">
-      <div class="col-md-12">
-        <div class="col-md-1">
-        </div>
-        <div class="col-md-10 outline">
-          <div class="col-md-1 mt-0">
-            <h1 class="big-text">1</h1>
-          </div>
-          <div class="col-md-11 mt-0">
-            <h3 class="text-left"> &#128198; Find a Court Date: </h3>
-            <h4>You can look through our schedule of appearances on our Calendar page, where you can seach either by Court or by Date. We generally appear in each court once every three months at this time, although larger Courts are seen more regularly. Have a look and see if our dates are convenient for you.</h4>
-            <a href="calendar.php" class="btn btn-primary">Search Calendar</a>
-          </div>
-        </div>
-        <div class="col-md-1">
-        </div>
+    <div class="col-md-3 attySidebar">
+      <h2 class="text-center">Appearing Attorneys</h2>
+      <hr>
+      <p class="text-center">OfCounselTN only uses experienced collections attorneys to appear in court on your behalf.
+        Below is a list of some of the seasoned attorneys we contract with.</p>
+      <hr>
+      <div class="col-md-12 nameTag">
+        <h4 class="text-center">Terry J. Canady, Esq.</h4>
       </div>
-      <div class="col-md-12">
-        <div class="col-md-1">
-        </div>
-        <div class="col-md-10 outline">
-          <div class="col-md-1 mt-0">
-            <h1 class="big-text">2</h1>
-          </div>
-          <div class="col-md-11 mt-0">
-            <h3 class="text-left"> &#10004; Make a Request:</h3>
-            <h4>Once you have found a convenient appearance, select the date to see the particular information for that appearance. From there, you can easily make a request to our office to confirm availability. You will immediately get a receipt email and should receive confirmation within one business day.</h4>
-          </div>
-        </div>
-        <div class="col-md-1">
-        </div>
+      <div class="col-md-12 nameTag">
+        <h4 class="text-center">Steven F. Glaser, Esq.</h4>
       </div>
-
-      <div class="col-md-12">
-        <div class="col-md-1">
-        </div>
-        <div class="col-md-10 outline">
-          <div class="col-md-1">
-            <h1 class="big-text">3</h1>
-          </div>
-          <div class="col-md-11 mt-0">
-            <h3 class="text-left"> <i class="fa fa-file-text-o" aria-hidden="true"></i> Set your cases:</h3>
-            <h4>After you have received confirmation from our office, make sure that you are using the proper warrant for your specific Court, then simply set your court date for the day you have confirmed with our office. Since you have confirmed the appearance with our office, we will notify you of any changes with the court date if any arise.</h4>
-          </div>
-        </div>
-        <div class="col-md-1">
-        </div>
+      <div class="col-md-12 nameTag">
+        <h4 class="text-center">Arthur Grisham, Esq.</h4>
       </div>
-      <div class="col-md-12">
-        <div class="col-md-1">
-        </div>
-        <div class="col-md-10 outline">
-          <div class="col-md-1 mt-0">
-            <h1 class="big-text">4</h1>
-          </div>
-          <div class="col-md-11 mt-0">
-            <h3 class="text-left"> 	<i class="fa fa-paper-plane" aria-hidden="true"></i> Send your docket:</h3>
-            <h4>At least two business days prior to our appearance on your behalf, please send us your finalized docket so our attorney can prepare for the court date. We do understand that sometimes the particulars of cases change between that time and the court date. You will be able to contact our office directly in that time period to notify us of the changes if necessary.</h4>
-
-          </div>
-        </div>
-        <div class="col-md-1">
-        </div>
+      <div class="col-md-12 nameTag">
+        <h4 class="text-center">Mike Hester, Esq.</h4>
       </div>
-      <div class="col-md-12">
-        <div class="col-md-1">
-        </div>
-        <div class="col-md-10 outline">
-          <div class="col-md-1">
-            <h1 class="big-text">5</h1>
-          </div>
-          <div class="col-md-11 mt-0">
-            <h3 class="text-left"> <i class="fa fa-gavel" aria-hidden="true"></i> Day of Court:</h3>
-            <h4>On the day of court, your office does not need to take any active steps to make sure your docket is handled appropriately. However, our attorney will have your office's information should they need to contact you for settlement or payment arrangement approval.</h4>
-          </div>
-        </div>
-        <div class="col-md-1">
-        </div>
+      <div class="col-md-12 nameTag">
+        <h4 class="text-center">Bill Shick, Esq.</h4>
       </div>
-      <div class="col-md-12">
-        <div class="col-md-1">
-        </div>
-        <div class="col-md-10 outline">
-          <div class="col-md-1">
-            <h1 class="big-text">6</h1>
-          </div>
-          <div class="col-md-11 mt-0">
-            <h3 class="text-left"> <i class="fa fa-reply" aria-hidden="true"></i> Get Results:</h3>
-            <h4>After we have appeared for your firm, we will compile the results from court and email them to your office. We know that this is a time sensitive matter and usually you will see your results within two business days. At this time, an invoice will be sent to you for our sevices.</h4>
-          </div>
-        </div>
-        <div class="col-md-1">
-        </div>
+      <div class="col-md-12 nameTag">
+        <h4 class="text-center">Eric Sitler, Esq.</h4>
       </div>
     </div>
   </section>
